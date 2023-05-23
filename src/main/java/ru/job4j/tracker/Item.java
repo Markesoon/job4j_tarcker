9d36787e43c41edc7b5b058f7337226a84a1a3e8
Markesoon <saiferdk@mail.ru>
2023-05-23 21:10:38 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,10 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item(int id) {
         this.id = id;
@@ -15,6 +17,10 @@ public class Item {
 
     public Item() {
 
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
