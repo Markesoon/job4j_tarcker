c9bcaee4e3cc97c27ca89fc2760c8713261184c9
Markesoon <saiferdk@mail.ru>
2023-05-30 18:37:11 +0300

    3. Массивы и модели. [#174892 # [#174892]]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,26 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book code = new Book("Clean code", 10);
+        Book garry = new Book("garry", 4);
+        Book mythiccal = new Book("mythiccal", 19);
+        Book cry = new Book("cry", 5);
+        Book[] prods = new Book[4];
+        prods[0] = code;
+        prods[1] = garry;
+        prods[2] = mythiccal;
+        prods[3] = cry;
+        for (int index = 0; index < prods.length; index++) {
+            Book pr = prods[index];
+            System.out.println(pr.getName() + " - " + pr.getCount());
+        }
+        System.out.println("0 and 3");
+        prods[0] = cry;
+        prods[3] = code;
+        for (int index = 0; index < prods.length; index++) {
+            Book pr = prods[index];
+            System.out.println(pr.getName() + " - " + pr.getCount());
+        }
+    }
+}
