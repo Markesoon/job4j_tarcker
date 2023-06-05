f58a3bcebe78e01d68dde2a549b8270d761a1598
Markesoon <saiferdk@mail.ru>
2023-06-05 15:24:08 +0300

    5. Tracker - хранилище [#396 # [#396 #420177]]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -8,23 +8,6 @@ public class Item {
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Item item = (Item) o;
-        return id == item.id && Objects.equals(name, item.name) && Objects.equals(created, item.created);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, created);
-    }
-
     public Item(int id) {
         this.id = id;
     }
@@ -40,12 +23,6 @@ public class Item {
 
     public Item(String name) {
         this.name = name;
-    }
-
-    public Item(int id, String name, LocalDateTime created) {
-        this.id = id;
-        this.name = name;
-        this.created = created;
     }
 
     public LocalDateTime getCreated() {
