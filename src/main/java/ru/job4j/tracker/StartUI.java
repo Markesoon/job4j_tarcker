15974210831cb124f58435009cb26d2d6262285d
Markesoon <saiferdk@mail.ru>
2023-07-12 16:12:31 +0300

    6.1. Разрыв зависимости StartUI от Scanner. [#181778 # [#181778 #428807]]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,6 +10,7 @@ public class StartUI {
         System.out.println("Добавленная заявка: " + item);
     }
 
+
     public static void showAllItem(Input input, Tracker tracker) {
         System.out.println("=== Show all items ===");
         Item[] items = tracker.findAll();
