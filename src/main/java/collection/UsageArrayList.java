5df7dab8819d2dceca45f9b4d60a9ed2d469578f
Markesoon <saiferdk@mail.ru>
2023-09-16 20:11:58 +0300

    2. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/collection/UsageArrayList.java
@@ -0,0 +1,16 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Stepan");
+        names.add("Ivan");
+        names.add("Petr");
+        for (String str : names) {
+            System.out.println(str);
+        }
+    }
+}
