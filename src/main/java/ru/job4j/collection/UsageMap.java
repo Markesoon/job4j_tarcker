37cbf4b8335704aedb497466d690bf410a7dcb00
Markesoon <saiferdk@mail.ru>
2023-10-08 19:10:27 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("gidvar@mail.ru", "Vasya Petrov");
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("palikin@gmail.ru", "Ivan Palikin");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
