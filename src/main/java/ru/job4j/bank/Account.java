78496cad4127631f2c12a448fa3d1deff71612f6
Markesoon <saiferdk@mail.ru>
2023-12-08 20:21:46 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,30 +2,67 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс модель банковского счёта.
+ */
 public class Account {
+    /**
+     * поле Реквизиты банковского счёта.
+     */
     private String requisite;
+    /**
+     * поле баланс счёта.
+     */
     private double balance;
 
+    /**
+     * Конструктор
+     * @param requisite реквизиты
+     * @param balance   баланс
+     */
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
+    /**
+     * Getter поля реквизит
+     * @return значение реквизита
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * Setter поля реквизит
+     * @param requisite значение реквизита
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * Getter поля баланс
+     * @return значение баланс
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Setter поля баланс
+     * @param balance значение баланс
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
+
+    /**
+     * Переопределение метода equals()
+     * @param o иной экземпляр класса Account
+     * @return true если Account равны,
+     * false если Account не равны
+     */
 
     @Override
     public boolean equals(Object o) {
@@ -39,6 +76,10 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * Переопределение метода hashCode()
+     * @return хэш-код
+     */
     @Override
     public int hashCode() {
         return Objects.hash(requisite);
--- src/main/java/ru/job4j/bank/User.java
+++ src/main/java/ru/job4j/bank/User.java
@@ -2,31 +2,67 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * класс представляющий клента.
+ */
 public class User {
+    /**
+     * Поле пасспорт клиента
+     */
     private String passport;
+    /**
+     * поле имени клиента
+     */
     private String username;
 
+    /**
+     * Конструктор
+     * @param passport данные паспорта клиента
+     * @param username Имя клиента
+     */
     public User(String passport, String username) {
         this.passport = passport;
         this.username = username;
     }
 
+    /**
+     * Getter поля паспорта
+     * @return Знаяение паспорта
+     */
     public String getPassport() {
         return passport;
     }
 
+    /**
+     * Setter поля паспортю
+     * @param passport Знаяение паспорта
+     */
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * Getter поля имени
+     * @return Знаяение имени
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Setter поля имени
+     * @param username Знаяение имени
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Переопределение метода equals()
+     * @param o иной экземпляр класса User
+     * @return true если User равны,
+     * false если User не равны
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +75,10 @@ public class User {
         return Objects.equals(passport, user.passport);
     }
 
+    /**
+     * Переопределение метода hashCode()
+     * @return хэш-код
+     */
     @Override
     public int hashCode() {
         return Objects.hash(passport);
