c5f2aa62d47dfde7ce885eac7dae769cfd951077
Markesoon <saiferdk@mail.ru>
2024-02-07 14:48:05 +0300

    2.2. Функциональный интерфейс [#247497]

--- src/main/java/ru/job4j/lambda/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/lambda/FunctionalInterfaces.java
@@ -9,39 +9,27 @@ import java.util.function.*;
 public class FunctionalInterfaces {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
-        BiConsumer<Integer, String> biConsumer = (key, value) -> map.put(key, value);
-        /*
-            Замените вставку элементов в карту на использование BiConsumer, он уже объявлен, требуется его реализовать.
-         */
-        map.put(1, "one");
-        map.put(2, "two");
-        map.put(3, "three");
-        map.put(4, "four");
-        map.put(5, "five");
-        map.put(6, "six");
-        map.put(7, "seven");
+        BiConsumer<Integer, String> biConsumer = map::put;
+        biConsumer.accept(1, "one");
+        biConsumer.accept(2, "two");
+        biConsumer.accept(3, "three");
+        biConsumer.accept(4, "four");
+        biConsumer.accept(5, "five");
+        biConsumer.accept(6, "six");
+        biConsumer.accept(7, "seven");
 
         BiPredicate<Integer, String> biPredicate = (key, value) -> key % 2 == 0 || value.length() == 4;
             for (Integer key : map.keySet()) {
-                System.out.println("key: " + key + " value: " + map.get(key));
-            /*
-                Замените проверку в if() на использование BiPredicate, он объявлен выше, требуется его реализовать.
-             */
+                if (biPredicate.test(key, map.get(key))) {
+                    System.out.println("key: " + key + " value: " + map.get(key));
+                }
             }
-        /*
-            Заменить создание ArrayList из значений Map на Supplier, объявлен ниже, требуется его реализовать.
-         */
+
         Supplier<List<String>> supplier = () -> new ArrayList<>(map.values());
         Consumer<String> consumer = System.out::println;
         Function<String, String> function = String::toUpperCase;
         for (String string : supplier.get()) {
             consumer.accept(function.apply(string));
-            System.out.println(string);
-            /*
-                Заменить вывод строк на применение Consumer
-                Заменить преобразование строк к строкам в верхнем регистре с помощью Function
-                Необходимое объявлено выше, требуется их реализовать.
-            */
         }
     }
 }
